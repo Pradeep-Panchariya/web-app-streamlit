a54ce1ac74721bdb69995f8dfd681d664ac390ab
Pradeep Panchariya <panchariya11@gmail.com>
2023-02-23 14:03:29 +0000

    add code for understanding

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import streamlit as st
 import pandas as pd
-def print_hello():
-    st.text("hello again")
-st.title('Hello this is a web app')
-st.button("Test",on_click=print_hello)
+from datetime import datetime
+
+##title with color and emoji
+#emoji : https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
+st.title(":green[Hello, Let's build something interesting stuff] :smile:")
+
+
+#subheader
+st.subheader("This is a sub header")
+
+
+#select_slider
+import streamlit as st
+
+selected_value = st.select_slider(
+    'Select a value',
+    options=['Option 1', 'Option 2', 'Option 3']
+)
+
+st.write('You selected:', selected_value)
